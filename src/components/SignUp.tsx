b5cab8987a45791d2f392e491123cf915909fc71
Richard Rodriguez <richardmanuel811@gmail.com>
2023-11-13 16:51:16 -0500

    - finishing route interceptor - beggining database modeling

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,12 +3,12 @@ import { Icons } from './Icons'
 import Link from 'next/link'
 import UserAuthForm from './UserAuthForm'
 
-const SignIn = () => {
+const SignUp = () => {
   return (
     <div className='container mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
         <Icons.Logo className='mx-auto h-6 w-6'/>
-        <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
+        <h1 className='text-2xl font-semibold tracking-tight'>Sign Up</h1>
         <p className='text-sm max-w-xs mx-auto'>
           By continuing, you are setting up a Breadit account and agree to our User Agreement and Privacy Policy.
         </p>
@@ -17,8 +17,8 @@ const SignIn = () => {
         <UserAuthForm />
 
         <p className='px-8 text-center text-sm text-zinc-700'>
-          New to Breadit?{' '}
-          <Link href='/sign-up' className='hover:text-zinc-800 text-sm underline underline-offset-4'> Sign up</Link>
+          Already a bredittor?{' '}
+          <Link href='/sign-in' className='hover:text-zinc-800 text-sm underline underline-offset-4'> Sign In</Link>
         </p>
 
       </div>
@@ -26,4 +26,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+export default SignUp
